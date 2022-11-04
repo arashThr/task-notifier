579f45eb6e3f3fe9e280a5a55061534a4056ee2d
Arash Taher <arashThr@outlook.com>
2022-11-04 23:11:26 +0100

    Send notifications

---
+++ README.md
@@ -0,0 +1,7 @@
+# Task notifier
+
+It's a simple notifier that helps me to keep track of the tasks I'm focused on.
+
+Running `npm start 5 "Create the READMEfile"' &` will start a process that after 5 minutes shows me a notification that my time is up.  
+It will helps me to keep track of the tasks I did during the day.
+
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,41 @@
-console.log('Hello')
+import notifier from 'node-notifier'
+import path from 'node:path'
+import { appendFile } from 'node:fs/promises'
+
+const historyFile = 'tasks_history.md'
+
+function timeout(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+async function startTimer(duration: number, desc: string) {
+  const start = new Date()
+  await timeout(duration * 1000)
+  const iconPath = path.join(__dirname, 'task.jpg')
+  console.log(iconPath)
+  notifier.notify({
+    message: desc,
+    actions: 'Mark as done',
+    sound: true,
+    wait: true,
+    timeout: 30,
+    icon: iconPath
+  }, (err, response, meta) => {
+    if (err)
+      throw err
+    console.log(response)
+    console.log(meta?.activationValue)
+    const outcome =
+      response === 'timeout' ? 'TIMEOUT' :
+      response === 'activate' && meta?.activationValue === 'Mark as done' ? 'DONE' :
+      response === 'closed' || meta?.activationValue === undefined ? 'FAILED' : 'UNKNOWN'
+    appendFile(historyFile, `${start.toLocaleString()} - ${duration}m (${outcome}) => ${desc}\n`)
+  })
+}
+
+const [duration, ...desc] = process.argv.slice(2)
+if (duration && desc)
+  startTimer(parseInt(duration), desc.join(' '))
+else
+  console.error('Require duration and description')
+
